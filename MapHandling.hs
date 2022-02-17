4f15bf4d0458df981f721447651d96754e85d5ff
Vincent Andersson <vinc.anders@gmail.com>
2022-02-17 14:06:58 +0100

    Complete specifications of MapHandling and Objects interfaces

--- MapHandling.hs
+++ MapHandling.hs
@@ -20,7 +20,7 @@ data TempTile = Temp TileBit | Void deriving Eq
 
 {- newMap w h
    Generates an empty map of entered width w and entered height h.
-   PRECONS: any positive integers w and h
+   PRECONS: Any positive integers w and h
    RETURNS: The representation of an empty map of the specified size
    EXAMPLE: newMap 5 5 = [[((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void)],[((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void)],[((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void)],[((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void)],[((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void), (('_', False), Void), ((' ', False), Void)]]
             Which when printed is representet as a grid of underscore signs '_' and spaces ' '.
@@ -82,23 +82,34 @@ rowToString ((x, Void):xs)   = fst x : rowToString xs
 rowToString ((x, Temp y):xs) = fst y : rowToString xs
 
 {- printSection map x y radius
-   
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Prints a section of the map within the radius of cardinal (Taxi-cab)
+   coordinates of the point provided.
+   PRECONS: Any Valid non negative coordinates within the maps bounds.
+   RETURNS: -
+   EXAMPLE: (The printed representations are used instead of the list form of the maps)
+                         " _ _ _ _ _ "
+                         " _ P _ _ _ "          " P _ _ "
+            printSection " _ _ _ _ _ " 2 2 1 -> " _ _ _ "
+                         " _ _ _ _ _ "          " _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: Prints the specified section of the map to the terminal row by row.
 -}
 printSection :: Map -> Int -> Int -> Int -> IO ()
 printSection map = printSectionAux map 0
 
 {- printSectionAux map acc x y radius
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Auxillary function, meant to be ran from printSection
+   PRECONS: Any Valid non negative coordinates within the maps bounds and an accumulator value at 0.
+   RETURNS: -
+   EXAMPLE: (The printed representations are used instead of the list form of the maps)
+                         " _ _ _ _ _ "
+                         " _ P _ _ _ "            " P _ _ "
+            printSection " _ _ _ _ _ " 0 2 2 1 -> " _ _ _ "
+                         " _ _ _ _ _ "            " _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: Prints the specified section of the map to the terminal row by row.
 -}
 printSectionAux :: Map -> Int -> Int -> Int -> Int -> IO ()
 printSectionAux ([], h)   i x y r = putStrLn ""
@@ -109,56 +120,87 @@ printSectionAux (m:ap, h) i x y r
      print (' ' : take (4*r+1) (drop (2*x-2*r-1) (rowToString m)) ++ " ")
      printSectionAux (ap, h) (i+1) x y r
 
-{- editMap Map x y c
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+{- editMap Map x y tile
+   Edits the map at the specified position to the specified tile.
+   PRECONS: A valid non negative coordinate within the maps bounds.
+   RETURNS: The edited map.
+   EXAMPLE: editMap (newMap 5 5) 2 2 (('O', False), Temp ('P', True)) ->
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ P _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: -
 -}
 editMap :: Map -> Int -> Int -> Tile -> Map
-editMap ([], h) _ _ _      = ([], h)
-editMap (r:ows, h) x y obj = editMapAux (r:ows, h) ([], h) 0 (2 * x + 1) y obj
+editMap ([], h) _ _ _       = ([], h)
+editMap (r:ows, h) x y tile = editMapAux (r:ows, h) ([], h) 0 (2 * x + 1) y tile
 
+{- editMapAux oldMap newMap acc x y tile
+   Auxillary function meant to be ran from editMap
+   PRECONS: A valid non negative coordinate within the maps bounds
+            and an accumulator at ([], h) and an accumulator at 0
+            where h is the height of the final map.
+   RETURNS: The edited map.
+   EXAMPLE: editMapAux (newMap 5 5) ([], 5) 0 2 2 (('O', False), Temp ('P', True)) ->
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ P _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: -
+-}
 editMapAux :: Map -> Map -> Int -> Int -> Int -> Tile -> Map
-editMapAux ([], _)   new y x0 y0 obj = new
-editMapAux (o:ld, h) new y x0 y0 obj
-  | y == y0    = (newMap ++ [editMapWidth o obj x0] ++ ld , h)
-  | otherwise  = editMapAux (ld, h) (newMap ++ [o], h) (y + 1) x0 y0 obj
+editMapAux ([], _)   new y x0 y0 tile = new
+editMapAux (o:ld, h) new y x0 y0 tile
+  | y == y0    = (newMap ++ [editMapWidth o tile x0] ++ ld , h)
+  | otherwise  = editMapAux (ld, h) (newMap ++ [o], h) (y + 1) x0 y0 tile
   where newMap = fst new
         editMapWidth :: MapRow -> Tile -> Int -> MapRow
-        editMapWidth []  obj _ = []
-        editMapWidth map obj w = take w map ++ obj : drop (w + 1) map
+        editMapWidth []  tile _ = []
+        editMapWidth map tile w = take w map ++ tile : drop (w + 1) map
 
 --editMapWidth (m:ap) obj 0 = obj : ap
 --editMapWidth (m:ap) obj w = m : editMapWidth ap obj (w - 1)
 
-{-
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+{- editMapTemp map x y TempTile
+   Edits the temp value of the tile att he specifed location.
+   PRECONS: A valid non negative coordinate within the maps bounds.
+   RETURNS: The map with the temptile value edited at the specified location.
+   EXAMPLE: editMapTemp (newMap 5 5) 2 2 Temp ('P', True) ->
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ P _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: -
 -}
 editMapTemp :: Map -> Int -> Int -> TempTile -> Map
 editMapTemp ([], h) _ _ _       = ([], h)
-editMapTemp (r:ows, h) x y temp = editMapAuxTemp (r:ows, h) ([], h) 0 (2 * x + 1) y temp
+editMapTemp (r:ows, h) x y temp = editMapTempAux (r:ows, h) ([], h) 0 (2 * x + 1) y temp
 
-{-
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+{- editMapTempAux oldMap newMap aux x y temp
+   Auxillary function meant to be ran from editMapTemp.
+   PRECONS: A valid non negative coordinate within the maps bounds and an accumulator 
+   ([], h) where h is the height of the final map and an accumulator y which is initialized 0.
+   RETURNS: The map with the temptile value edited at the specified location.
+   EXAMPLE: editMapTempAux (newMap 5 5) ([], h) 0 2 2 Temp ('P', True) ->
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ P _ _ "
+                         " _ _ _ _ _ "
+                         " _ _ _ _ _ "
+   VARIANT: length map
+   SIDE EFFECTS: -
 -}
-editMapAuxTemp :: Map -> Map -> Int -> Int -> Int -> TempTile -> Map
-editMapAuxTemp ([], _)   new y x0 y0 temp = new
-editMapAuxTemp (o:ld, h) new y x0 y0 temp
+editMapTempAux :: Map -> Map -> Int -> Int -> Int -> TempTile -> Map
+editMapTempAux ([], _)   new y x0 y0 temp = new
+editMapTempAux (o:ld, h) new y x0 y0 temp
   | y == y0    = (newMap ++ [editMapWidthTemp o temp x0] ++ ld , h)
-  | otherwise  = editMapAuxTemp (ld, h) (newMap ++ [o], h) (y + 1) x0 y0 temp
+  | otherwise  = editMapTempAux (ld, h) (newMap ++ [o], h) (y + 1) x0 y0 temp
   where newMap = fst new
         editMapWidthTemp :: MapRow -> TempTile -> Int -> MapRow
         editMapWidthTemp []  temp _ = []
@@ -175,37 +217,44 @@ editMapWidthTemp (m:ap) temp w      = m : editMapWidthTemp ap temp (w - 1)
 -}
 
 {- readMap map x y
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Returns the tile value at the location 
+   PRECONS: A valid non negative coordinate within the maps bounds.
+   RETURNS: The tile at the location of (x, y) on the map.
+   EXAMPLE: readMap (newMap 5 5) 3 3 = (('_', False), Void) 
+   VARIANT: -
+   SIDE EFFECTS: - 
 -}
 readMap :: Map -> Int -> Int -> Tile
 readMap ([], h)  x y = ((' ', False), Void)
 readMap (map, h) x y = map !! max 0 y !! max 0 (2*x+1)
 
 {- hasTemp tile
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Returns True if the specified tile has a temp value which would take precedence
+   over the regular value.
+   PRECONS: Any valid Tile.
+   RETURNS: True if the tile has a none Void temp value, false otherwise
+   EXAMPLE: hasTemp (('_', False), ('P', True)) = True
+            hasTemp (('A', True),  Void)        = False
+   VARIANT: -
+   SIDE EFFECTS: -
 -}
 hasTemp :: Tile -> Bool
 hasTemp (_, Void) = False
 hasTemp (_, _)    = True
 
 {- getCollision tile
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Returns True if the tile is occupied / would collide with something were to move into the tile.
+   PRECONS: Any valid Tile.
+   RETURNS: True if the tile is occupied, either by the temp value or the base tile. Where the temp
+            tile takes precedence.
+   EXAMPLE: getCollision (('_', False), ('P', True)) = True
+            getCollision (('A', True),  Void)        = True
+            getCollision (('P', True), ('_', False)) = False
+   VARIANT: -
+   SIDE EFFECTS: -
 -}
 getCollision :: Tile -> Bool 
 getCollision ((tile, col), Void)  = col
 getCollision (_, Temp (temp, tempCol)) = tempCol
+
+
--- Object.hs
+++ Object.hs
@@ -1,46 +1,71 @@
 module Object where
 
-import MapHandling as MH 
+import MapHandling as MH
 
 type Position  = (Int, Int)
 
 {- Direction
-
+   Represents a direction or lack of direction (Void)
 -}
 data Direction = N | NE | E | SE | S | SW | W | NW | Void deriving Eq
 
-{- movePos
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+-- TODO REMAKE WITH TRY CATCH STRUCTURE
+{- movePos (x0, y0) (x1, y1) map
+   Moves the temporary tile from the first position to the second. 
+   This overwrites the value at the second position and leaves the 
+   first tile with a Void temp value.
+   PRECONS: Two non negative coordinates that are within the bounds
+            of the map.
+   RETURNS: The map with the specified tile moved.
+   EXAMPLE: (The printed representations are used instead of the list form of the maps)
+                                 " _ _ _ _ _ "    " _ _ _ _ _ "
+                                 " _ P _ _ _ "    " _ _ _ _ _ "
+              move (1, 1) (3, 2) " _ _ _ _ _ " -> " _ _ _ P _ "
+                                 " _ _ _ _ _ "    " _ _ _ _ _ "
+                                 " _ _ _ _ _ "    " _ _ _ _ _ "
+   VARIANT: -
+   SIDE EFFECTS: -
 -}
 movePos :: Position -> Position -> Map -> Map
 movePos (x0, y0) (x1, y1) map = MH.editMapTemp (MH.editMapTemp map x1 y1 (snd oldPos)) x0 y0 MH.Void
   where oldPos = MH.readMap map x0 y0
-{- move 
 
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+-- TODO REMAKE WITH TRY CATCH STRUCTURE
+{- move (x0, y0) dir map
+   Moves the temp value from the specified tile one step in the specied direction, does not move if Void.
+   PRECONS: A valid coordinate to the tile and a valid direction where the tile in the direction is within
+            the maps bounds.
+   RETURNS: The map with the specified tile moved .
+   EXAMPLE: (The printed representations are used instead of the list form of the maps)
+                             " _ _ _ _ _ "    " _ _ _ _ _ "
+                             " _ P _ _ _ "    " _ _ P _ _ "
+               move (1, 1) E " _ _ _ _ _ " -> " _ _ _ _ _ "
+                             " _ _ _ _ _ "    " _ _ _ _ _ "
+                             " _ _ _ _ _ "    " _ _ _ _ _ "
+
+                             " _ _ _ _ _ "    " _ P _ _ _ "
+                             " _ P _ _ _ "    " _ _ _ _ _ "
+               move (1, 1) N " _ _ _ _ _ " -> " _ _ _ _ _ "
+                             " _ _ _ _ _ "    " _ _ _ _ _ "
+                             " _ _ _ _ _ "    " _ _ _ _ _ "
+   VARIANT: -
+   SIDE EFFECTS: -
 -}
-move :: Position -> Direction -> Map -> Map 
-move (x0, y0) dir map = MH.editMap (MH.editMap map x1 y1 (MH.readMap map x0 y0)) x0 y0 (('_', False), MH.Void)
+move :: Position -> Direction -> Map -> Map
+move (_, _)  Object.Void map = map
+move (x0, y0) dir map        = MH.editMapTemp (MH.editMapTemp map x1 y1 (snd (MH.readMap map x0 y0))) x0 y0 MH.Void
   where numDir = directionalValue dir
         y1     = y0 + fst numDir
         x1     = x0 + snd numDir
 
 {- directionalValue dir
-
-   PRECONS: 
-   RETURNS: 
-   EXAMPLE: 
-   VARIANT: 
-   SIDE EFFECTS: 
+   Returns the coordinate values of an entered direction. Void returns (0, 0)
+   PRECONS: Any valid direction
+   RETURNS: the pair of coordinates representing the change in coordinates from moving said direction.
+   EXAMPLE: directionalValue N    == (-1, 0)
+            directionalValue Void == (0, 0)
+   VARIANT: -
+   SIDE EFFECTS: - 
 -}
 directionalValue :: Direction -> Position
 directionalValue dir
@@ -54,6 +79,29 @@ directionalValue dir
   | dir == NW = (-1, -1)
   | otherwise = ( 0,  0)
 
+{- collides (x, y) map
+   Returns wether the specified position on the map is occupied and would collide with entering values.
+   PRECONS: A valid non negative coordinate within the maps bounds.
+   RETURNS: True if it is occupied / solid, False if it is not.
+   EXAMPLE: (The printed representations are used instead of the list form of the maps)
+                           " _ _ _ _ _ "
+                           " _ _ _ _ _ "
+           collides (3, 3) " _ _ _ _ _ " = True
+                           " _ _ _ P _ "
+                           " _ _ _ _ _ "
+
+                           " _ _ _ _ _ "
+                           " _ _ _ _ _ "
+           collides (0, 0) " _ _ _ _ _ " = False
+                           " _ _ _ P _ "
+                           " _ _ _ _ _ "
+   VARIANT: -
+   SIDE EFFECTS: -
+-}
+collides :: Position -> Map -> Bool
+collides (x, y) map = MH.getCollision (MH.readMap map x y)
+
+
 {- 
 
    PRECONS: 
@@ -62,5 +110,3 @@ directionalValue dir
    VARIANT: 
    SIDE EFFECTS: 
 -}
-collides :: Position -> Map -> Bool 
-collides (x, y) map = MH.getCollision (MH.readMap map x y)
